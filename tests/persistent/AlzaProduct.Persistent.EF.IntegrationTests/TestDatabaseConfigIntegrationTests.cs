e4ae41441bd25d46a2adcd4d1f0821e5d60bdcd3
Hepatica <danil.tkachenko@getmore.cz>
2024-09-21 22:37:06 +0200

    Adjust TEST_DATABASE_CONNECTION_STRING in appsettings.json

--- tests/persistent/AlzaProduct.Persistent.EF.IntegrationTests/TestDatabaseConfigIntegrationTests.cs
+++ tests/persistent/AlzaProduct.Persistent.EF.IntegrationTests/TestDatabaseConfigIntegrationTests.cs
@@ -6,8 +6,8 @@ public class TestDatabaseConfigIntegrationTests
     [Fact]
     public void GetTestConnectionString_ShouldReturnValidConnectionString()
     {
-        var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_CONNECTION_STRING"); ;
-        //var connectionString = TestDatabaseConfig.GetTestConnectionString();
+        //var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_CONNECTION_STRING"); ;
+        var connectionString = TestDatabaseConfig.GetTestConnectionString();
 
         Assert.False(string.IsNullOrEmpty(connectionString), "Test Db Connection string should not be null or empty");
 
