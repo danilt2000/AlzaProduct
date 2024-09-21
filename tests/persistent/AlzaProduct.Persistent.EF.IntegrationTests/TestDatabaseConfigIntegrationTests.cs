d473c26d0dee57a2ac1cee8cee52dcc0b05ab9e5
Hepatica <danil.tkachenko@getmore.cz>
2024-09-21 22:26:44 +0200

    Implement test getting TEST_DATABASE_CONNECTION_STRING

--- tests/persistent/AlzaProduct.Persistent.EF.IntegrationTests/TestDatabaseConfigIntegrationTests.cs
+++ tests/persistent/AlzaProduct.Persistent.EF.IntegrationTests/TestDatabaseConfigIntegrationTests.cs
@@ -6,9 +6,11 @@ public class TestDatabaseConfigIntegrationTests
     [Fact]
     public void GetTestConnectionString_ShouldReturnValidConnectionString()
     {
-        var connectionString = TestDatabaseConfig.GetTestConnectionString();
+        var connectionString = Environment.GetEnvironmentVariable("TEST_DATABASE_CONNECTION_STRING"); ;
+        //var connectionString = TestDatabaseConfig.GetTestConnectionString();
 
         Assert.False(string.IsNullOrEmpty(connectionString), "Test Db Connection string should not be null or empty");
+
 
         Assert.Contains("Test", connectionString);
     }
